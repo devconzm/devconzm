93ec30610deb238c53ddaf2802761bcc69057dca
Olivier JM <manolivier93@gmail.com>
2019-03-05 16:24:11 +0200

    changed the title

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,7 +30,7 @@ function Header(){
                             <a className="nav-link" href="#about">About</a>
                         </li>
                         <li className="nav-item">
-                            <a className="btn btn-primary" href="#devcon">DevCon18</a>
+                            <a className="btn btn-outline-primary" href="#devcon">DevCon18</a>
                         </li>
 
                     </ul>
