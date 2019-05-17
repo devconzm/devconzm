e9d127b642c494a566f91cfda537e77b27c57915
Chizzo Cheese <chizzo84@gmail.com>
2019-05-17 16:35:33 +0200

    Added Venue Section
    Added Venue Section above footer and relevant styling

--- src/components/Venue.js
+++ src/components/Venue.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from 'react';
+import theGrandHotelLogo from "../assets/dev4.png";
 
 function Venue() {
     return (
         <section id="venue">
             <div className="container text-center">
+				<img src={theGrandHotelLogo} alt="THE GRAND HOTEL MAIN HALL" title="THE GRAND HOTEL MAIN HALL" />
 				<p>THE GRAND HOTEL</p>
 				<p>MAIN HALL</p>
 				<p>08am - 5pm</p>
-				<p><a href="#header" className="btn btn-light text-dark">ENTER EMAIL</a></p>
-				<p><a href="#header" className="btn btn-outline-light text-white">REGISTER NOW</a></p>
+				<p><a href="#header" className="btn btn-light btn-lg email">ENTER EMAIL</a></p>
+				<p><a href="#header" className="btn btn-outline-light text-white register">REGISTER NOW</a></p>
             </div>
         </section>
     )
