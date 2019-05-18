6332de9356fd1cde1e9d684a58434e9ab9ec4b1f
Chizzo Cheese <chizzo84@gmail.com>
2019-05-18 23:38:13 +0200

    Fixed section width and added Partners section
    - Edited CSS to have slimmer sections
    - Added partners section

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ function App() {
           </div>
         </section>
       </Fragment>
+      <Partners />
       <Venue />
       <Footer />
     </Fragment>
--- src/components/Venue.js
+++ src/components/Venue.js
@@ -4,13 +4,15 @@ import theGrandHotelLogo from "../assets/dev4.png";
 function Venue() {
     return (
         <section id="venue">
-            <div className="container text-center">
-				<img src={theGrandHotelLogo} alt="THE GRAND HOTEL MAIN HALL" title="THE GRAND HOTEL MAIN HALL" />
-				<p>THE GRAND HOTEL</p>
-				<p>MAIN HALL</p>
-				<p>08am - 5pm</p>
-				<p><a href="#header" className="btn btn-light btn-lg email">ENTER EMAIL</a></p>
-				<p><a href="#header" className="btn btn-outline-light text-white register">REGISTER NOW</a></p>
+            <div className="container">
+                <div className="text-center">
+                    <img src={theGrandHotelLogo} alt="THE GRAND HOTEL MAIN HALL" title="THE GRAND HOTEL MAIN HALL" />
+                    <p>THE GRAND HOTEL</p>
+                    <p>MAIN HALL</p>
+                    <p>08am - 5pm</p>
+                    <p><a href="#header" className="btn btn-light btn-lg email">ENTER EMAIL</a></p>
+                    <p><a href="#header" className="btn btn-outline-light text-white register">REGISTER NOW</a></p>
+                </div>
             </div>
         </section>
     )
