bd77a6483d85fa6f6ac97e1fb3d9845262546dee
Olivier JM <manolivier93@gmail.com>
2019-05-18 22:59:47 +0200

    added back the venue
    @Chizzoz  check this and let me know if it is what you intended?

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Partners from './components/Partners'
 import Speakers from './components/Speakers'
 import Tickets from './components/Tickets'
 import About from './components/About'
+import Venue from './components/Venue'
 import Footer from './components/Footer'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
@@ -29,6 +30,7 @@ function App() {
           </div>
         </section>
       </Fragment>
+      <Venue />
       <Footer />
     </Fragment>
   )
