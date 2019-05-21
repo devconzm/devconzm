ba994b1f25c87bec4cf6d3ca480c8e17963c2bbd
ikayz <ikayz@live.com>
2019-05-21 10:16:29 +0200

    Add row and grid for extra small, medium, and large screens

--- src/components/About.js
+++ src/components/About.js
@@ -4,11 +4,15 @@ function About() {
   return (
     <section id="about">
       <div className="container">
-        <h1 className='text-center'>About</h1>
-        <div className="">
-          {/* TODO */}
-          <h4>What is Lorem Ipsum?</h4>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <div className="row">
+          <div className="col-xs-12 col-md-12 col-lg-12">
+            <h1 className='text-center'>About</h1>
+            <div className="">
+            {/* TODO */}
+            <h4>What is Lorem Ipsum?</h4>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          </div>
+        </div>
         </div>
       </div>
     </section>
