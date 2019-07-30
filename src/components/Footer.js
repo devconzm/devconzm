12ec5fbe388ca5e1591754e4b72a967287dd9aff
Brittani Gongre <brittani.gongre@assurant.com>
2019-07-30 12:47:38 -0400

    added email message at bottom of partners page, added social media links in the footer, and cleaned up some more code.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { FaFacebookSquare, FaTwitterSquare, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 
 function Footer() {
     return (
         <footer className="page-footer font-small">
+            {/* Social Media */}
+            <div className="social-media">
+                <a href="https://m.facebook.com/devconzm/">
+                    <FaFacebookSquare 
+                        size={50}
+                    />
+                </a>
+                <a href="https://twitter.com/devcon_zm">
+                    <FaTwitterSquare 
+                        size={50}
+                    />
+                </a>
+                <a href="https://www.linkedin.com/company/developer-conference-zambia-19">
+                    <FaLinkedin 
+                        size={50}
+                    />
+                </a>
+                <a href="mailto:info@devcon.co.zm">
+                    <FaEnvelope 
+                        size={50}
+                    />
+                </a>
+                <div>
+                    <h6>Check us out on social media!</h6>
+                </div>
+            </div>
+            {/* Social Media */}
 
             {/* Copyright */}
-            <div className="container footer-copyright text-center py-3">&copy; <a href="http://devcon.co.zm">2019 DevCon Zambia.</a> All rights reserved.
+            <div className="container footer-copyright text-center py-3">
+                <p>&copy;2019 <a href="http://devcon.co.zm"> DevCon Zambia</a> all rights reserved.</p>
             </div>
             {/* Copyright */}
 
             {/* Code of Conduct */ } 
-            < div className = "container footer-copyright text-center py-3" > <a  target="_blank" href = "https://github.com/devconzm/devconzm/blob/master/CODE_OF_CONDUCT.md" > Code of Conduct </a> </div >
+            <div className="container footer-copyright text-center py-3">
+                <a target="_blank" href = "https://github.com/devconzm/devconzm/blob/master/CODE_OF_CONDUCT.md" >Code of Conduct</a>
+            </div>
             {/* Code of Conduct */ }
 
         </footer>
--- src/components/Partners.js
+++ src/components/Partners.js
@@ -1,73 +1,86 @@
-import React from 'react'
+import React from "react";
 
 function Partners() {
   return (
     <section id="partners">
       <div className="container">
-        <h1 className='text-center section-title'>Partners</h1>
+        <h1 className="text-center section-title">Partners</h1>
         <div className="">
           {/* TODO */}
-            <div className = "container" >
-                <div className = "row" >
-                    <div className="col-sm" >
-                        <a href="https://www.jetbrains.com" target="_blank">
-                            <img
-                                width="200"
-                                height="200"
-                                alt="JetBrains"
-                                src={
-                                    require('../assets/jetbrains.png')
-                                }
-                            />
-                        </a>
-                    </div>
-                    <div className="col-sm" >
-                        <a href="https://www.stickermule.com" target="_blank">
-                            <img width="200" height="200" alt="Stickermule"
-                                src={
-                                    require('../assets/stickermule.svg')
-                                }
-                            />
-                        </a>
-                    </div>
-                    <div className = "col-sm" >
-                        <a href = "#"
-                            target = "_blank" >
-                            <img
-                                width = "200"
-                                height = "200"
-                                alt = "JetBrains"
-                                src = {
-                                    require('../assets/agoracode.png')
-                                }
-                            />
-                        </a>
-                    </div>
-                    <div className="col-sm" >
-                        <a href="https://www.gitkraken.com" target="_blank">
-                            <img width="200" height="200" alt="GitKraken"
-                                src={
-                                    require('../assets/gitkraken.svg')
-                                }
-                            />
-                        </a>
-                    </div>
-                    <div className="col-sm" >
-                        <a href="https://www.bongohive.co.zm" target="_blank">
-                            <img width="200" height="200" alt="Bongohive"
-                                src={
-                                    require('../assets/bongohive.png')
-                                }
-                            />
-                        </a>
-                    </div>
-                    <div className = "col-sm"></div>
-
+          <div className="container">
+            <div className="row">
+              <div className="col-sm">
+                <a href="https://www.jetbrains.com" target="_blank">
+                  <img
+                    width="200"
+                    height="200"
+                    alt="JetBrains"
+                    src={require("../assets/jetbrains.png")}
+                  />
+                </a>
+              </div>
+              <div className="col-sm">
+                <a href="https://www.stickermule.com" target="_blank">
+                  <img
+                    width="200"
+                    height="200"
+                    alt="Stickermule"
+                    src={require("../assets/stickermule.svg")}
+                  />
+                </a>
+              </div>
+              <div className="col-sm">
+                <a href="#" target="_blank">
+                  <img
+                    width="200"
+                    height="200"
+                    alt="JetBrains"
+                    src={require("../assets/agoracode.png")}
+                  />
+                </a>
+              </div>
+              <div className="col-sm">
+                <a href="https://www.gitkraken.com" target="_blank">
+                  <img
+                    width="200"
+                    height="200"
+                    alt="GitKraken"
+                    src={require("../assets/gitkraken.svg")}
+                  />
+                </a>
+              </div>
+              <div className="col-sm">
+                <a href="https://www.bongohive.co.zm" target="_blank">
+                  <img
+                    width="200"
+                    height="200"
+                    alt="Bongohive"
+                    src={require("../assets/bongohive.png")}
+                  />
+                </a>
+              </div>
+              {/* Below section is kept for styling purposes */}
+              {/* Section is preset to add new partner */}
+              <div className="col-sm" />
+              <a href="#" target="_blank">
+                <img 
+                    width="200" 
+                    height="200" 
+                    alt="" 
+                    src="" 
+                />
+              </a>
             </div>
           </div>
         </div>
       </div>
+      <div className="center email-header">
+        <h6>
+          If you would like to partner with us, send us an email at{" "}
+          <a href="mailto:info@devcon.co.zm">DevConZM</a>
+        </h6>
+      </div>
     </section>
-  )
+  );
 }
 export default Partners;
