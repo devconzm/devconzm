96e050bdd416956864cbdbe650719e8d39a2e74e
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2019-08-07 04:24:37 +0200

    modified ticket section

--- src/components/Tickets.js
+++ src/components/Tickets.js
@@ -2,28 +2,64 @@ import React from "react";
 
 function Tickets() {
   return (
-    <section id='tickets'>
-      <div className='container'>
-        <h1 className='text-center'>Tickets</h1>
-        <div className='tickets row text-center'>
-          <div className="ticket ticket-1 col-sm box-shadow">
-            <h4 className="ticket-header">EARLY-BIRD PASS</h4>
-            <p className="ticket-price">K 50</p>
-            <p className="ticket-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </div>
-          <div className="ticket ticket-2 col-sm box-shadow">
-            <h4 className="ticket-header">STUDENT PASS</h4>
-            <p className="ticket-price">K 80</p>
-            <p className="ticket-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id mauris ex.</p>
-          </div>
-          <div className="ticket ticket-3 col-sm box-shadow">
-            <h4 className="ticket-header">REGULAR PASS</h4>
-            <p className="ticket-price">K 100</p>
-            <p className="ticket-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id mauris ex.</p>
+    <section class="pricing py-5">
+      <div class="container">
+    <div class="row">
+   
+      <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">EARLY-BIRD PASS</h5>
+            <h6 class="card-price text-center">K50<span class="period"></span></h6>
+            <hr></hr>
+            <ul class="fa-ul">
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Have access To Morning Session</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>All Key Notes</li>
+            
+            </ul>
+            
+            <a href="#" class="btn btn-block btn-primary text-uppercase">Get Tickets</a>
           </div>
         </div>
       </div>
-    </section>
+      
+      <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">STUDENT PASS</h5>
+            <h6 class="card-price text-center">K80<span class="period">/</span></h6>
+            <hr></hr>
+            <ul class="fa-ul">
+              <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>Have access to Lemited Sessions</strong></li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>All Key Notes</li>
+             
+            </ul>
+           
+            <a href="#" class="btn btn-block btn-primary text-uppercase">Get Tickets</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">REGULAR PASS</h5>
+            <h6 class="card-price text-center">$100<span class="period"></span></h6>
+            <hr></hr>
+            <ul class="fa-ul">
+              <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>attend all sessions</strong></li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>All Key Notes</li>
+             
+            </ul>
+            
+            <a href="#" class="btn btn-block btn-primary text-uppercase">Get Tickets</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </section>
+  
   );
 }
 
