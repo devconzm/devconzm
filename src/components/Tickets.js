144d8b98b8ae2b240ed749bdf7a680986a01f202
Olivier JM <manolivier93@gmail.com>
2019-08-07 20:51:16 +0200

    replaced available with Get to temporary fit the text

--- src/components/Tickets.js
+++ src/components/Tickets.js
@@ -9,18 +9,13 @@ function Tickets() {
           <div className="ticket ticket-1 col-sm box-shadow">
             <h4 className="ticket-header">EARLY-BIRD PASS</h4>
             <p className="ticket-price">K 150</p>
-            <p className="ticket-content">Available from 29th July to 19th August 2019.</p>
+            <p className="ticket-content">Get from 29th July to 19th August 2019</p>
             <a href="https://qkt.io/I3SHAd"><p className="btn btn-info btn-sm">Click here to Purchase.</p></a>
           </div>
-          {/*<div className="ticket ticket-2 col-sm box-shadow">
-            <h4 className="ticket-header">STUDENT PASS</h4>
-            <p className="ticket-price">K 80</p>
-            <p className="ticket-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id mauris ex.</p>
-  </div>*/}
           <div className="ticket ticket-2 col-sm box-shadow">
             <h4 className="ticket-header">REGULAR PASS</h4>
             <p className="ticket-price">K 250</p>
-            <p className="ticket-content">Available from 20th August to 20th September 2019.</p>
+            <p className="ticket-content">Get from 20th August to 20th September 2019</p>
             {/*<a href="https://qkt.io/I3SHAd"><p className="btn btn-info btn-sm">Click here to Purchase.</p></a>*/}
           </div>
         </div>
