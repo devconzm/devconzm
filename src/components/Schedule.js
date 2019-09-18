6450ab9db2f08b5b7617723f1697f3ad80ec58e3
Beardless-sheik <44676055+Beardless-sheik@users.noreply.github.com>
2019-09-18 20:34:12 +0200

    Update Schedule.js
    Added basic styling and done some alignment on the UI

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from "react";
 import { FaTwitterSquare, FaGithubSquare } from "react-icons/fa";
 import scheduleData from "../data/schedule.json";
-import Header from "./Header.js";
+import Info from "./Info";
 import Footer from "./Footer.js";
 
 
@@ -10,8 +10,8 @@ export class Schedule extends Component {
     // honestly this isn't the best solution, If it can be optimized further give it a go
     const [day1, day2] = scheduleData.schedule;
     return (
-      <div className="container">
-        <Header />
+      <div>
+        <Info />
         <Fragment>
           <h1>{day1.day}</h1>
           {day1.schedule.map(schedule => (
@@ -34,53 +34,55 @@ export class Schedule extends Component {
 
 function ScheduleItem({ schedule, id }) {
   return (
-    <div id="accordion" key={id}>
-      <div className="container">
-        <div className="card-body border-primary">
-          <a
-            className="row card-link text-decoraton-none"
-            href={"#collapseOne" + id}
-            data-toggle="collapse"
-          >
-            <div className="col-md-1">
-              <img
-                src={schedule.image}
-                className="rounded-circle"
-                width={80}
-                height={80}
-                alt="keynote speaker"
-              />
-              <br />
-              <br />
-              <a href={schedule.githubLink} title="github">
-                <FaGithubSquare size={20} />
-              </a>
-              <a href="https://twitter.com/Olivierjmm?s=17" title="twitter">
-                <FaTwitterSquare size={20} />
-              </a>
-            </div>
-            <div className="col-md-8 text-center">
-                <h2>{schedule.speaker}</h2>
-                <p>{schedule.topic}</p>
-            </div>
-            <div className="col-md-2 text-right">
-              <span>{schedule.time}</span> 
+    <div className="container card border-primary">
+      <div id="accordion" key={id}>
+        <div className="container">
+          <div className="card-body border-primary">
+            <a
+              className="row card-link text-decoraton-none"
+              href={"#collapseOne" + id}
+              data-toggle="collapse"
+            >
+              <div className="col-md-1">
+                <img
+                  src={schedule.image}
+                  className="rounded-circle"
+                  width={80}
+                  height={80}
+                  alt="keynote speaker"
+                />
                 <br />
-              <span>60mins</span>
+                <br />
+                <a href={schedule.githubLink} title="github">
+                  <FaGithubSquare size={20} />
+                </a>
+                <a href="https://twitter.com/Olivierjmm?s=17" title="twitter">
+                  <FaTwitterSquare size={20} />
+                </a>
+              </div>
+              <div className="col-md-8 text-center">
+                  <h2>{schedule.speaker}</h2>
+                  <p>{schedule.topic}</p>
+              </div>
+              <div className="col-md-2 text-right">
+                <span>{schedule.time}</span> 
+                  <br />
+                <span>60mins</span>
+              </div>
+            </a>
+          </div>
+          <div
+            id={"collapseOne" + id}
+            className="collapse"
+            data-parent="#accordion"
+          >
+            <div className="card-footer text-secondry">
+              {schedule.description}
             </div>
-          </a>
-        </div>
-        <div
-          id={"collapseOne" + id}
-          className="collapse"
-          data-parent="#accordion"
-        >
-          <div className="card-footer text-secondry">
-            {schedule.description}
           </div>
         </div>
+        <br />
       </div>
-      <br />
     </div>
   );
 }
