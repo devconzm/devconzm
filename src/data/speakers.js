e2c2091e6976bc02efb967cfd47547e5a7a17680
Chizzo Cheese <chizzo84@gmail.com>
2019-10-13 20:47:42 +0200

    Image optimisation and and changing image reference

--- src/data/speakers.js
+++ src/data/speakers.js
@@ -2,35 +2,35 @@ export const speakers = [
   {
     id: 1,
     name: "Cobus Bernard ",
-    image: "/img/speakers/corbus.jpg",
+    image: "/img/speakers/corbus.webp",
     company: "Senior Technical Evangelist -AWS Sub-Saharan Africa",
     talk: "Modern App Development in the Cloud, Why Service Meshes?"
   },
   {
     id: 2,
     name: "Rahul Sharma",
-    image: "/img/speakers/rahul.jpg",
+    image: "/img/speakers/rahul.webp",
     company: "Agile Coach at Think Agile",
     talk: "Know it better, to facilitate it better"
   },
   {
     id: 3,
     name: " Dennis Mubamba",
-    image: "/img/speakers/dennis.jpg",
+    image: "/img/speakers/dennis.webp",
     company: "Senior Software Engineer - Mika Express",
     talk: "Actors systems and Asynchronous Software Development"
   },
   {
     id: 4,
     name: "Twaambo Haamucenje",
-    image: "/img/speakers/twaambo.jpg",
+    image: "/img/speakers/twaambo.webp",
     company: "CTO - Mvesesani Media",
     talk: "Building a Zambian Company and Staying Sane"
   },
   {
     id: 5,
     name: "Sekayi Fundafunda Lungu",
-    image: "/img/speakers/sekayi.jpg",
+    image: "/img/speakers/sekayi.webp",
     company: "Campus Innovation Lead - BongoHive",
     talk:
       "Creativity for Technology: An interactive session on finding your niche"
@@ -39,7 +39,7 @@ export const speakers = [
   {
     id: 6,
     name: "Mwaamba Alec Mugala",
-    image: "/img/speakers/mwaamba.jpg",
+    image: "/img/speakers/mwaamba.webp",
     company: "Director - EpicArts Studios",
     talk: "The Game Industry - It is time For Africa"
   },
@@ -47,14 +47,14 @@ export const speakers = [
   {
     id: 7,
     name: "Chinedu Koggu",
-    image: "img/speakers/chinedu.jpg",
+    image: "img/speakers/chinedu.webp",
     company: "CTO - Probase Limited",
     talk: "Innovative Product Development from Conception to launch"
   },
   {
     id: 8,
     name: "Shakerrie Almond",
-    image: "img/speakers/shakerrie.jpg",
+    image: "img/speakers/shakerrie.webp",
     company: "Chief Operations Officer - BroadPay",
     talk: "TBD"
   },
